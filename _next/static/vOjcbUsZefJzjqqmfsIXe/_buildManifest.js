b6d941f8427c3071d328245b0a2a7acd02ba7433
dasta <vickydasta@gmail.com>
2023-05-22 22:16:01 +0700

    add stuff

---
+++ _next/static/vOjcbUsZefJzjqqmfsIXe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-b4c2a6e895a61d4bb99f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8c9950b7e5bae95c3919.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
